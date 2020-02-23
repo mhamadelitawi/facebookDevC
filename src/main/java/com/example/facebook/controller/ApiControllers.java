a0f4c096f6e0e1f2c5fde972cd47422eec0eaac8
Mhamad El Itawi <mhamadelitawi@Mhamads-MacBook-Pro.local>
2020-02-24 00:44:15 +0200

    list channels and playlists

--- src/main/java/com/example/facebook/controller/ApiControllers.java
+++ src/main/java/com/example/facebook/controller/ApiControllers.java
@@ -3,14 +3,21 @@ package com.example.facebook.controller;
 import com.example.facebook.database.mySQL.UserManagerMySql;
 import com.example.facebook.manager.ControllerManager;
 import com.example.facebook.manager.DatabaseManager;
-import com.example.facebook.manager.UserManager;
 import com.example.facebook.model.User;
+import com.example.facebook.youtube.Quickstart;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.Channel;
+import com.google.api.services.youtube.model.ChannelListResponse;
+import com.google.api.services.youtube.model.PlaylistListResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 @Controller
 public class ApiControllers {
@@ -24,6 +31,56 @@ public class ApiControllers {
     public String test()
     {
         return "test";
+
+    }
+
+    @GetMapping("/youTubeTest")
+    @ResponseBody
+    public String youTubeTest(
+            @RequestParam(name="part", required=false) String part,
+            @RequestParam(name="isMine", required=false) Boolean isMine,
+            @RequestParam(name="id", required=false) String id,
+            @RequestParam(name="channelId", required=false) String channelId
+
+    )
+
+
+            throws GeneralSecurityException, IOException {
+
+        if(part == null )
+            part = "snippet,contentDetails,statistics,brandingSettings";
+
+
+         YouTube youtubeService = Quickstart.getYouTubeService();
+//
+//        YouTube.Channels.List request = youtubeService.channels()
+//                .list(part);
+//
+//        if(id != null)
+//         request.setId(id); //PLgca0gRh5aLcNX_31uwCfu1oYXzfwhGbg
+//
+//        if(isMine != null)
+//            request.setMine(isMine);
+//
+//        ChannelListResponse response = request.execute();
+
+
+        YouTube.Playlists.List request = youtubeService.playlists().list(part);
+
+        if(isMine != null)
+             request.setMine(isMine);
+
+        if(channelId != null)
+            request.setChannelId(channelId); //UCm-MDGAMQxN2sc38fOBC7mA
+
+        //request.setFields("items/contentDetails,nextPageToken,pageInfo");
+        PlaylistListResponse response = request.execute();
+
+
+
+
+        System.out.println(response);
+        return response.toString();
 
     }
 
---
+++ src/main/java/com/example/facebook/youtube/Quickstart.java
@@ -0,0 +1,137 @@
+package com.example.facebook.youtube;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.YouTubeScopes;
+import com.google.api.services.youtube.model.Channel;
+import com.google.api.services.youtube.model.ChannelListResponse;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Quickstart {
+
+    public static String jsonFileLocation = "/static/client_secret.json";
+
+    /** Application name. */
+    private static final String APPLICATION_NAME = "API Sample";
+
+    /** Directory to store user credentials for this application. */
+    private static final java.io.File DATA_STORE_DIR = new java.io.File( System.getProperty("user.home"), ".credentials/youtube-java-quickstart");
+
+    /** Global instance of the {@link FileDataStoreFactory}. */
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY =
+        JacksonFactory.getDefaultInstance();
+
+    /** Global instance of the HTTP transport. */
+    private static HttpTransport HTTP_TRANSPORT;
+
+
+    private static final List<String> SCOPES =
+        Arrays.asList(YouTubeScopes.YOUTUBE_READONLY);
+
+
+
+
+
+    public static Credential authorize() throws IOException, GeneralSecurityException {
+
+
+        HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+
+
+        // Load client secrets.
+        InputStream in = Quickstart.class.getResourceAsStream(jsonFileLocation);
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow =
+                new GoogleAuthorizationCodeFlow.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(DATA_STORE_FACTORY)
+                .setAccessType("offline")
+                .build();
+        Credential credential = new AuthorizationCodeInstalledApp(
+            flow, new LocalServerReceiver()).authorize("user");
+        return credential;
+    }
+
+
+
+    public static YouTube getYouTubeService() throws IOException, GeneralSecurityException {
+        Credential credential = authorize();
+        return new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+
+
+
+
+
+
+    public static void testYoutube() throws IOException, GeneralSecurityException {
+        YouTube youtube = Quickstart.getYouTubeService();
+        try {
+            YouTube.Channels.List channelsListByUsernameRequest = youtube.channels().list("snippet,contentDetails,statistics");
+            channelsListByUsernameRequest.setForUsername("GoogleDevelopers"); //GoogleDevelopers
+
+            ChannelListResponse response = channelsListByUsernameRequest.execute();
+            Channel channel = response.getItems().get(0);
+            System.out.printf( "This channel's ID is %s. Its title is '%s', and it has %s views.\n", channel.getId(),
+                    channel.getSnippet().getTitle(),  channel.getStatistics().getViewCount());
+        } catch (GoogleJsonResponseException e) {
+            e.printStackTrace();
+            System.err.println("There was a service error: " +
+                    e.getDetails().getCode() + " : " + e.getDetails().getMessage());
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+
+    }
+
+
+
+
+//    public static void main(String[] args) throws IOException {
+//        YouTube youtube = getYouTubeService();
+//        try {
+//            YouTube.Channels.List channelsListByUsernameRequest = youtube.channels().list("snippet,contentDetails,statistics");
+//            channelsListByUsernameRequest.setForUsername("GoogleDevelopers");
+//
+//            ChannelListResponse response = channelsListByUsernameRequest.execute();
+//            Channel channel = response.getItems().get(0);
+//            System.out.printf(
+//                "This channel's ID is %s. Its title is '%s', and it has %s views.\n",
+//                channel.getId(),
+//                channel.getSnippet().getTitle(),
+//                channel.getStatistics().getViewCount());
+//        } catch (GoogleJsonResponseException e) {
+//            e.printStackTrace();
+//            System.err.println("There was a service error: " +
+//                e.getDetails().getCode() + " : " + e.getDetails().getMessage());
+//        } catch (Throwable t) {
+//            t.printStackTrace();
+//        }
+//    }
+}
